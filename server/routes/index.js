2a6fd4941e2bd9b645a420183709d91dbbaf3f3e
Mikheil Mukhiguli <mikheil.mukhiguli@azry.com>
2021-01-18 18:06:11 +0400

    product rendering

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,13 +2,22 @@ var express = require("express");
 var router = express.Router();
 var { public } = require("../config/config");
 var userController = require("../controllers/userController");
+var Product = require("../models/product");
 
 router.use("/register", require("./registerRouter.js"));
 router.use("/login", require("./loginRouter.js"));
 router.use("/newProduct", require("./newProductRouter.js"));
 
 router.get("/", function (req, res) {
-  res.sendFile(public + "/index.html");
+  Product.find({}, function (err, products) {
+    var productMap = {};
+
+    products.forEach(function (product) {
+      productMap[product._id] = product;
+    });
+
+    res.render(public + "/index.html", { items: products });
+  });
 });
 
 router.get("/logout", function (req, res) {
@@ -33,6 +42,24 @@ router.post("/passwordRecovery", (req, res, next) => {
 
 router.post("/password", (req, res, next) => {
   userController.changePassword(req, res, next);
+});
+
+router.get("/something", (req, res, next) => {
+  Product.find({}, function (err, products) {
+    var productMap = [];
+
+    products.forEach(function (product) {
+      productMap.push({
+        ...product,
+        img: {
+          contentType: product.img.contentType,
+          data: product.img.data.toString("base64"),
+        },
+      });
+    });
+
+    res.json(productMap);
+  });
 });
 
 module.exports = router;
