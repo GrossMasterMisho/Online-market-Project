50e6c3deea5319b9c905cfb4fed33ddc9ae9e4a5
Mikheil Mukhiguli <mikheil.mukhiguli@azry.com>
2021-02-15 01:50:50 +0400

    changed host

--- server/config/config.js
+++ server/config/config.js
@@ -3,4 +3,4 @@ var path = require("path");
 const public = path.join(__dirname, "/../../public/views");
 
 module.exports.public = public;
-module.exports.host = "localhost:3000";
+module.exports.host = "online-market-project.herokuapp.com";
